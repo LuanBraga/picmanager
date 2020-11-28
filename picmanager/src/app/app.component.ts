3cc80f12b3fcaae6f1b4f6fb7b1dc1245b0b9f17
Berserker <luanbraga.lb@gmail.com>
2020-11-28 11:21:05 -0300

    add photo component

--- picmanager/src/app/app.component.ts
+++ picmanager/src/app/app.component.ts
@@ -6,7 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hell';
-  url = 'https://artsy-media-uploads.s3.amazonaws.com/-VBWvHn1Skz8LVzmcrTD4w%2Fhellthumb800.jpg';
-  description = "hell party";
 }
---
+++ picmanager/src/app/photo/photo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'lb-photo',
+    templateUrl: 'photo.component.html'
+})
+
+export class PhotoComponent {
+    title = 'hell';
+    description = 'hell party';
+    url= 'http://www.ghibli.jp/gallery/howl012.jpg';
+}
